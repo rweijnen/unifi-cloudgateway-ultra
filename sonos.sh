fd44c5038a9cb6ca76355c0c6ece7976a4d85cb8
Remko Weijnen <r.weijnen@gmail.com>
2024-02-28 15:18:37 +0100

    Update sonos.sh

--- sonos.sh
+++ sonos.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # requires multicast-relay.py script, place it in /data/on_boot.d/settings/
+# wget https://github.com/alsmith/multicast-relay/raw/master/multicast-relay.py
 # br0 i my devices lan, br20 is my IoT VLAN...
 python3 /data/on_boot.d/settings/multicast-relay.py  --noMDNS --interfaces br0 br20
